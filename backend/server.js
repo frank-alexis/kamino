de4caa0e502e2154af7758891d29c744c392f47b
Frank Alexis <huamanalexis44@gmail.com>
2026-07-12 21:41:36 -0500

    Corregir ruta comodín para Express

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,13 @@ app.use(cors());
 app.use(express.json());
 
 
+// --- CONFIGURACIÓN PARA EL FRONTEND ---
 app.use(express.static(path.join(__dirname, '../frontend')));
 
-app.get('*', (req, res) => {
+// Cambia app.get('*', ...) por esto:
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
 });
-// ----------------------------------------------
 
 const apiRoutes = require('./routes/api');
 app.use('/api', apiRoutes);
